67e5072aaafd318ea049c0579c76432dc08a7bd6
Leonid <leonidb93@gmail.com>
2018-10-30 02:47:19 +0100

    detect if the angel is seen by the player

---
+++ Assets/_Scripts/AngelController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngelController : MonoBehaviour {
+    private Renderer renderer;
+
+    private void Start() {
+        renderer = GetComponent<Renderer>();
+    }
+    void Update() {
+
+        //Vector3 screenPoint = Camera.main.WorldToViewportPoint(transform.position);
+        //bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 
+        //    && screenPoint.y > 0 && screenPoint.y < 1;
+        //print(screenPoint.y);
+        //if (onScreen) print("On screen");
+        //else print("Not on screen");
+
+        if (renderer.IsVisibleFrom(Camera.main)) Debug.Log("Visible");
+        else Debug.Log("Not visible");
+    }
+
+
+}
