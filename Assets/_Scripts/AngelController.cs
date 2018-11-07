a00fe737a9b3e2781ba6821ffadcf31f3f440c61
Leonid <leonidb93@gmail.com>
2018-11-07 23:44:28 +0100

    start implement pathfinding

--- Assets/_Scripts/AngelController.cs
+++ Assets/_Scripts/AngelController.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AngelController : MonoBehaviour {
     private Renderer renderer;
 
+    // Path finding
+    public Transform goal;
+    private NavMeshAgent agent;
+
     private void Start() {
         renderer = GetComponent<Renderer>();
+        agent = transform.parent.GetComponent<NavMeshAgent>();
+
+        
     }
     void Update() {
-
+        agent.destination = goal.position;
         //Vector3 screenPoint = Camera.main.WorldToViewportPoint(transform.position);
         //bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 
         //    && screenPoint.y > 0 && screenPoint.y < 1;
@@ -17,6 +25,7 @@ public class AngelController : MonoBehaviour {
         //if (onScreen) print("On screen");
         //else print("Not on screen");
 
+        // IsVisibleFrom is a method from the RendererExtensions.cs script
         if (renderer.IsVisibleFrom(Camera.main)) Debug.Log("Visible");
         else Debug.Log("Not visible");
     }
