76a42cfd0fce467660c6d533cfa932b39e77601a
LB31 <leonidb93@gmail.com>
2018-11-14 15:26:41 +0100

    add house, add key, implement functionality to pick up items

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public List<GameObject> collectableItems;
+    public List<string> collectedItems;
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+    public float yaw = 0.0f;
+    public float pitch = 0.0f;
+
+    void Start() {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update() {
+        Movement();
+
+    }
+
+
+
+    private void OnTriggerStay(Collider other) {
+    if (Input.GetKey("e")) {
+        foreach (GameObject item in collectableItems) {
+            if (other.gameObject == item) {
+                collectedItems.Add(other.name);
+                Destroy(other.gameObject);
+            }
+        }
+    }
+    }
+
+
+    void Movement() {
+
+        if (controller.isGrounded) {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection = Camera.main.transform.TransformDirection(moveDirection);
+            moveDirection = moveDirection * speed;
+
+            if (Input.GetButton("Jump")) {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+#if UNITY_EDITOR
+        yaw += speed * Input.GetAxis("Mouse X");
+        pitch -= speed * Input.GetAxis("Mouse Y");
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+#endif
+
+        // Apply gravity
+        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+
+        // Move the controller
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
