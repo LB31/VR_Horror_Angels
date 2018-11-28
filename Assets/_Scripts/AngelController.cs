d44842a9150cab0ef373cd95c39db25de004758a
Leonid <leonidb93@gmail.com>
2018-11-28 01:42:58 +0100

    continue with angel AI, start optimize performance

--- Assets/_Scripts/AngelController.cs
+++ Assets/_Scripts/AngelController.cs
@@ -4,28 +4,38 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class AngelController : MonoBehaviour {
-    private Renderer renderer;
+    public Renderer[] AllRenderers;
+    public Renderer OwnRenderer;
 
     // Path finding
     public Transform goal;
     private NavMeshAgent agent;
 
     private void Start() {
-        renderer = GetComponent<Renderer>();
-        agent = transform.parent.GetComponent<NavMeshAgent>();
+        goal = FindObjectOfType<PlayerController>().transform;
+        agent = GetComponent<NavMeshAgent>();
 
-        
+        AllRenderers = GetComponentsInChildren<Renderer>();
+        OwnRenderer = GetComponent<Renderer>();
+
+
     }
     void Update() {
-        
+
         // IsVisibleFrom is a method from the RendererExtensions.cs script
-        if (renderer.IsVisibleFrom(Camera.main)) {
+        if (OwnRenderer.IsVisibleFrom(Camera.main)) {
             Debug.Log("Visible");
+            foreach (Renderer renderer in AllRenderers) {
+                renderer.enabled = true;
+            }
             agent.enabled = false;
         } else {
             Debug.Log("Not visible");
             agent.enabled = true;
             agent.destination = goal.position;
+            foreach (Renderer renderer in AllRenderers) {
+                renderer.enabled = false;
+            }
         }
     }
 
